3a021201d91745e5bcd1bc07f804b749be92c71e
Jatin Salve <jasalve_b19@el.vjti.ac.in>
2021-10-13 16:33:11 +0530

    Updates

---
+++ precache-manifest.07ea613268f3a72cee97db14633013b5.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dba2844022c6041b0a0bc00d1c22632",
+    "url": "/portfolio_x/index.html"
+  },
+  {
+    "revision": "f7afc84358f013063033",
+    "url": "/portfolio_x/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "c8f0936a903245e1f76d",
+    "url": "/portfolio_x/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "9d05c064a6578f0324df",
+    "url": "/portfolio_x/static/css/main.b57bef10.chunk.css"
+  },
+  {
+    "revision": "2194d3ac26bc446730ce",
+    "url": "/portfolio_x/static/js/2.8103befe.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/portfolio_x/static/js/2.8103befe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7afc84358f013063033",
+    "url": "/portfolio_x/static/js/3.b6a4d920.chunk.js"
+  },
+  {
+    "revision": "c8f0936a903245e1f76d",
+    "url": "/portfolio_x/static/js/4.6c737a57.chunk.js"
+  },
+  {
+    "revision": "9d05c064a6578f0324df",
+    "url": "/portfolio_x/static/js/main.c8da1071.chunk.js"
+  },
+  {
+    "revision": "b11e938f614b56522157",
+    "url": "/portfolio_x/static/js/runtime-main.b5452667.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio_x/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "88d3bbd4174a1bc9bb5da42f837d3594",
+    "url": "/portfolio_x/static/media/FREELANCE.88d3bbd4.png"
+  },
+  {
+    "revision": "c2534794f9f7b438e9a848de41ac4fae",
+    "url": "/portfolio_x/static/media/IMAGEP.c2534794.png"
+  },
+  {
+    "revision": "cbd194e880b024b626983e825a872e0c",
+    "url": "/portfolio_x/static/media/JOOMLA.cbd194e8.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio_x/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e9345a7fa6e7d2a8fe06b6973aa297b3",
+    "url": "/portfolio_x/static/media/SUMMER.e9345a7f.png"
+  },
+  {
+    "revision": "08fc0de4188f410af3413489030270ff",
+    "url": "/portfolio_x/static/media/VES.08fc0de4.png"
+  },
+  {
+    "revision": "1262d3112a54b14a36d532c3e6512f94",
+    "url": "/portfolio_x/static/media/VJTI.1262d311.png"
+  },
+  {
+    "revision": "2ff6822089786f063a2a321842995b5b",
+    "url": "/portfolio_x/static/media/ZOOM.2ff68220.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/portfolio_x/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio_x/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio_x/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio_x/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/portfolio_x/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
